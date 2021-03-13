f3e8cb95ef98c76c26eb772af8fb455d09f22d2a
SeminoleCasa <71413297+SeminoleCasa@users.noreply.github.com>
2021-03-13 07:52:24 -0500

    Update

--- Starter-Code/wallet.py
+++ Starter-Code/wallet.py
@@ -1,6 +1,11 @@
 import subprocess
 import json
 
+import os
+from constants import *
+
+mnemonic = "desert vibrant board skull miss need range jump dream black silk glide"
+
 command = './derive -g --mnemonic="INSERT HERE" --cols=path,address,privkey,pubkey --format=json'
 
 p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
